31828375a362606c245e50ea74cf943d5938a0e3
Jonathan <jonathan@jonathan-Inspiron-1545.(none)>
2012-09-24 11:48:16 +0200

    writing on the report

--- lifetime.py
+++ lifetime.py
@@ -8,24 +8,30 @@ import sys
 def computeLifetime(h,time,tmin,tmax):
 	Heff=[h[i] for i in range(len(h)) if tmax>time[i]>tmin]
 	timeEff=[t for t in time if tmax>t>tmin]
-
+	
 	A=sum(timeEff[i]*Heff[i] for i in range(len(Heff)))
 	B=sum(Heff)
 	def l(lambdac):
-		lambd=lambdac[0]
+		lambd=1./lambdac[0]
+		lambd2=1./2.03
 		c=lambdac[1]
-		return -sum(Heff[i]*(np.log(np.exp(-lambd*timeEff[i])+c**2)-np.log(
-		((np.exp(-lambd*s1)-np.exp(-lambd*s2))/lambd + c**2*(tmax-tmin)))) for i in range(len(Heff)))
+#		A=lambdac[2]**2
+#		c=np.sqrt(0.0015)
+		A=1
+		#print lambdac
+		return -sum(Heff[i]*(np.log(A*np.exp(-lambd*timeEff[i])+(1-A)*np.exp(-lambd2*timeEff[i])+c**2)-np.log(
+		(A*(np.exp(-lambd*tmin)-np.exp(-lambd*tmax))/lambd+(1-A)*(np.exp(-lambd2*tmin)-np.exp(-lambd2*tmax))/lambd2 + c**2*(tmax-tmin)))) for i in range(len(Heff)))
 
 	def lD(lambd):
 		return B/lambd-A+B*(s1*np.exp(-s1*lambd)-s2*np.exp(-s2*lambd))/(np.exp(-s1*lambd)-np.exp(-s2*lambd))
 	lambd=newton(lD,B/A)
-#	val=fmin(l,[B/A,0])
-#	print 1./lambd
-#	print 1./val[0],val[1]
-#	lambd=val[0]
+	val=fmin(l,[2.2,0.001])
+	print 1./lambd
+	print val
+	lambd=1./val[0]
 #	print "asdf"
-	return sum(time[i]*h[i] for i in range(len(h)))/sum(h),1./lambd,B*np.log(lambd)-A*lambd-B*np.log(np.exp(-tmin*lambd)-np.exp(-tmax*lambd)),B
+
+	return sum(time[i]*h[i] for i in range(len(h)))/sum(h),1./lambd,B*np.log(lambd)-A*lambd-B*np.log(np.exp(-tmin*lambd)-np.exp(-tmax*lambd)),B,val[1]
 	
 	
 	
@@ -33,11 +39,11 @@ def computeLifetime(h,time,tmin,tmax):
 
 if __name__=="__main__":
 	s1=2
-	s2=14
+	s2=15
 
 	delay=0.004
 	ct=[]
-	with open('lifetime') as f:
+	with open('lifetimeFinal') as f:
 		H=[]
 		for line in f:
 		    H.append(int(line.split()[1]))
@@ -45,21 +51,20 @@ if __name__=="__main__":
 		    if len(H)==8190:
 		        break
 
-	n=200
+	n=1000
 	ts=np.linspace(0,50,n+1)
 	N=[sum(H[j] for j in range(len(H)) if ts[i]<ct[j]<ts[i+1]) for i in range(n)]
 
 	#p.figure()
 	x=np.linspace(0.1,1,100)
 	#p.plot(x,[lD(i) for i in x])
-	lifetime1,lifetime,likelyhood,totalSamples=computeLifetime(H,ct,s1,s2)
-	c=0
+	lifetime1,lifetime,likelyhood,totalSamples,c=computeLifetime(H,ct,s1,s2)
 	print totalSamples
 	p.plot(reduce(lambda a,b:a+b,[[t]*2 for t in ts]),[0]+reduce(lambda a,b:a+b,[[float(iN)/totalSamples/ts[1]]*2 for iN in N])+[0],label="bins")
 	p.plot(reduce(lambda a,b:a+b,[[t]*2 for t in ts]),1./lifetime/(np.exp(-s1/lifetime)-np.exp(-s2/lifetime)+c**2*(s2-s1))*(np.exp(reduce(lambda a,b:a+b,[[t]*2 for t in ts])/(-lifetime))+c**2),label="analytic")
 #	p.plot(ct,H,label="raw")
-	
-	ensembleSize=20
+
+	ensembleSize=2
 	ensemblelifetime1=[0]*ensembleSize
 	ensemblelifetime=[0]*ensembleSize
 	ensembleLikelyhood=[0]*ensembleSize
@@ -72,7 +77,7 @@ if __name__=="__main__":
 			if s2>x>s1:
 				count+=1
 			H[int(timeToChannel(x)+0.5)]+=1
-		ensemblelifetime1[i],ensemblelifetime[i],ensembleLikelyhood[i],t=computeLifetime(H,ct,s1,s2)
+		ensemblelifetime1[i],ensemblelifetime[i],ensembleLikelyhood[i],t,t2=computeLifetime(H,ct,s1,s2)
 
 	#p.plot(ct,H,label="sampled")
 	p.legend(loc=1)
